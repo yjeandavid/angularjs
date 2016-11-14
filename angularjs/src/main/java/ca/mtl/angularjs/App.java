c8b2a2f2403ba4eef4456e4aa1f166703401a040
Jean-David <yjeandavid@gmail.com>
2016-11-14 18:39:26 -0500

    Create project angularjs

---
+++ angularjs/src/main/java/ca/mtl/angularjs/App.java
@@ -0,0 +1,13 @@
+package ca.mtl.angularjs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
